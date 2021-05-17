5c8ceba051abb9920fe0aa53a222be3e26a02106
selinahussain <selinahussain@live.com>
2021-05-17 11:42:38 +0100

    added tests and updated vehicle method with error handling

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -16,7 +16,9 @@ import scala.concurrent.ExecutionContext
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(ws: WSClient, val controllerComponents: ControllerComponents, implicit val ec: ExecutionContext) extends BaseController {
+class HomeController @Inject()(ws: WSClient,
+                               val controllerComponents: ControllerComponents,
+                               implicit val ec: ExecutionContext) extends BaseController {
 
   /**
    * Create an Action to render an HTML page.
@@ -26,17 +28,23 @@ class HomeController @Inject()(ws: WSClient, val controllerComponents: Controlle
    * a path of `/`.
    */
 
-  def index(vehicleName: String) = Action.async { implicit request: Request[AnyContent] =>
 
-    val futureResult = ws.url(s"http://localhost:9001/checkVehicle/${vehicleName}").get()
-
-    futureResult.map { response =>
-      val veh = Json.fromJson[Vehicle](response.json).get
-      Ok(views.html.vehicle(veh))
-
-    }
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
 
   }
+
+  def vehicle(vehicleName: String) = Action.async { implicit request: Request[AnyContent] =>
+    val futureResult = ws.url(s"http://localhost:9001/checkVehicle/${vehicleName}").get()
+    futureResult.map { response =>
+      val js = Json.fromJson[Vehicle](response.json)
+      val veh = js.get
+      Ok(views.html.vehicle(veh))
+    } recover {
+      case _ => NotFound
+    }
+  }
+
 
 
 }
--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -5,24 +5,26 @@ import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
 
+
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  *
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
-class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class HomeControllerSpec() extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
 
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
+//    "render the index page from a new instance of controller" in {
+//      val controller = new HomeController(stubControllerComponents())
+//      val home = controller.index().apply(FakeRequest(GET, "/"))
+//
+//      status(home) mustBe OK
+//      contentType(home) mustBe Some("text/html")
+//      contentAsString(home) must include ("Welcome to Play")
+//    }
 
     "render the index page from the application" in {
       val controller = inject[HomeController]
@@ -42,4 +44,26 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       contentAsString(home) must include ("Welcome to Play")
     }
   }
+
+  "HomeController GET/vehicle/vehiclename" should {
+    "render the vehicle page from the application" in {
+      val controller = inject[HomeController]
+      val vehicle = controller.vehicle("BMW").apply(FakeRequest(GET, "/"))
+
+      status(vehicle) mustBe OK
+      contentType(vehicle) mustBe Some("text/html")
+      contentAsString(vehicle) must include ("Welcome to Vehicle")
+    }
+
+    "fail to render the vehicle page from the application" in {
+      val controller = inject[HomeController]
+      val vehicle = controller.vehicle("test").apply(FakeRequest(GET, "/"))
+
+      status(vehicle) mustBe NOT_FOUND
+
+    }
+
+
+  }
+
 }
